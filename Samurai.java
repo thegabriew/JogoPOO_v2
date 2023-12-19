f70f3fa2a1ef5f9fa75eb7a0371a74483d6b7504
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-12-19 14:59:55 -0300

    Update Samurai.java

--- Samurai.java
+++ Samurai.java
@@ -1,45 +1,91 @@
+package Jogo;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Samurai extends Personagem {
 
-  public static final String ARMA_KATANA = "Katana";
-  public static final String ARMA_FOICE = "Foice";
+	  public static final String ARMA_KATANA = "Katana";
+	  public static final String ARMA_FOICE = "Foice";
+	  
 
-  public Samurai(String escolhaArma) {
-    super(escolhaArma);
-    inicializarArma(escolhaArma);
-  }
+	  public Samurai(String escolhaArma) {
+	    super(escolhaArma);
+	    inicializarArma(escolhaArma);
+	  }
 
-  private void inicializarArma(String escolhaArma) {
-    if (ARMA_KATANA.equals(escolhaArma)) {
-      // Configura ataques para Katana
-      setAtaque(75); // Ataque padrão
-    } else if (ARMA_FOICE.equals(escolhaArma)) {
-      // Configura ataques para Foice
-      setAtaque(50); // Ataque padrão
-    }
-  }
+	  private void inicializarArma(String escolhaArma) {
+	    if (ARMA_KATANA.equals(escolhaArma)) {
+	  
+	      setAtaque(50); 
+	    } else if (ARMA_FOICE.equals(escolhaArma)) {
+	      
+	      setAtaque(50); 
+	    }
+	  }
 
-  // Métodos específicos para cada ataque
-  public void laminaDaEscuridao() {
-    if (ARMA_KATANA.equals(getArma())) {
-      receberDano(75);
-    }
-  }
+	  // Ataques
+	  public void laminaDaEscuridao() {
+	    if (ARMA_KATANA.equals(getArma())) {
+	      receberDano(75);
+	    }
+	  }
 
-  public void retalhador() {
-    if (ARMA_KATANA.equals(getArma())) {
-      receberDano(50);
-    }
-  }
+	  public void retalhador() {
+	    if (ARMA_KATANA.equals(getArma())) {
+	      receberDano(50);
+	    }
+	  }
 
-  public void decapitacaoADistancia() {
-    if (ARMA_FOICE.equals(getArma())) {
-      receberDano(75);
-    }
-  }
+	  public void decapitacaoADistancia() {
+	    if (ARMA_FOICE.equals(getArma())) {
+	      receberDano(75);
+	    }
+	  }
 
-  public void corteDoCampones() {
-    if (ARMA_FOICE.equals(getArma())) {
-      receberDano(50);
-    }
-  }
-}
+	  public void corteDoCampones() {
+	    if (ARMA_FOICE.equals(getArma())) {
+	      receberDano(50);
+	    }
+	  }
+	  
+	  
+	  @Override
+	  public List <String> getArmas(){
+		  return Arrays.asList(ARMA_KATANA, ARMA_FOICE);
+	  }
+	  
+	  @Override
+	    public int atacar(int ataque, Personagem alvo) {
+	        if (ARMA_KATANA.equals(getArma())) {
+	            switch (ataque) {
+	                case 1:
+	                    alvo.receberDano(75);
+	                    return 75;
+	                case 2:
+	                    alvo.receberDano(50);
+	                    return 50;
+	                default:
+	                    throw new IllegalArgumentException("Ataque inválido");
+	            }
+	        } else if (ARMA_FOICE.equals(getArma())) {
+	            switch (ataque) {
+	                case 3:
+	                    alvo.receberDano(50);
+	                    return 75;
+	                case 4:
+	                    alvo.receberDano(75);
+	                    return 50;
+	                default:
+	                    throw new IllegalArgumentException("Ataque inválido");
+	            }
+	        } else {
+	            throw new IllegalStateException("Arma não reconhecida");
+	        }
+	    }
+
+	    @Override
+	    public List<String> getAtaques() {
+	        return Arrays.asList("1- Lâmina da escuridão", "2- Retalhador", "3- Corte do camponês", "4- Decapitação a distância");
+	    }
+	}
